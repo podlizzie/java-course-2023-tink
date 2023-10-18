71ceb71d71120ec43c6f5bb3bdb0b789982fae5e
lzbkl <lzbkln@mail.ru>
2023-10-18 00:59:16 -0500

    raw version of Task4

---
+++ src/main/java/edu/hw2/Task4/CreateCallingInfo.java
@@ -0,0 +1,24 @@
+package edu.hw2.Task4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateCallingInfo {
+    private CreateCallingInfo() {
+
+    }
+
+    public static List<CallingInfo> createCallingInfo() {
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        List<CallingInfo> callingInfoList = new ArrayList<>();
+
+        for (int i = 2; i < stackTrace.length; i++) {
+            StackTraceElement caller = stackTrace[i];
+            String className = caller.getClassName();
+            String methodName = caller.getMethodName();
+            callingInfoList.add(new CallingInfo(className, methodName));
+        }
+
+        return callingInfoList;
+    }
+}
---
+++ src/test/java/edu/hw2/Task4Test.java
@@ -0,0 +1,25 @@
+package edu.hw2;
+
+import edu.hw2.Task4.CallingInfo;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import static edu.hw2.Task4.CreateCallingInfo.createCallingInfo;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Task4Test {
+
+    @Test
+    public void testGetCallingInfo() {
+        List<CallingInfo> callingInfoList = createCallingInfo();
+        assertThat(callingInfoList).isNotEmpty();
+        assertThat(callingInfoList.get(0).className()).isEqualTo("edu.hw2.Task4Test");
+        assertThat(callingInfoList.get(0).methodName()).isEqualTo("getCallingInfo_ReturnsCallingInfo");
+    }
+
+    @Test
+    public void TestShouldContainAllCallingMethods() {
+        List<CallingInfo> callingInfoList = createCallingInfo();
+        assertThat(callingInfoList.size()).isGreaterThan(2);
+    }
+
+}
