72a5acf3f085afb51c9f70bb231007ce957ef9c4
lzbkl <lzbkln@mail.ru>
2023-11-03 23:14:50 -0500

    add README.md

--- src/main/java/edu/project2/utility/generation/Generation1.java
+++ src/main/java/edu/project2/utility/generation/Generation1.java
@@ -2,9 +2,10 @@ package edu.project2.utility.generation;
 
 import edu.project2.model.Maze;
 
-public class PerfectGeneration implements Generator {
+public class Generation1 implements Generator {
+
     /**
-     * Generates a perfect maze using height and width.
+     * Generates a maze using height and width.
      *
      * @param height the height of the maze
      * @param width  the width of the maze
