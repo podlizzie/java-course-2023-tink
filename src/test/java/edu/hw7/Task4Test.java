54fda133adfadfb3157cab594e02f9c10f94f8bd
lzbkl <lzbkln@mail.ru>
2023-11-27 00:51:37 -0600

    fix Task4 tests

--- src/test/java/edu/hw7/Task4Test.java
+++ src/test/java/edu/hw7/Task4Test.java
@@ -12,6 +12,25 @@ class Task4Test {
     private static final String AVERAGE_ACCELERATION_TEMPLATE = "Average acceleration: %.3f";
     private static final String ACCURACY_RESULT_TEMPLATE = "Accuracy for %d iterations: %.5f";
     private static final int COUNT_OF_THREADS = 4;
+    private static final int ITERATIONS10M = 10000000;
+    private static final int ITERATIONS100M = 100000000;
+    private static final int ITERATIONS1B = 1000000000;
+
+    @Test
+    void testThatAccuracyImprovesWithMoreIterations() {
+        // Given
+        // When
+        double accuracy1 = Math.abs(Math.PI - Task4.calculatePiMultithreaded(ITERATIONS10M, COUNT_OF_THREADS));
+        double accuracy2 = Math.abs(Math.PI - Task4.calculatePiMultithreaded(ITERATIONS100M, COUNT_OF_THREADS));
+        double accuracy3 = Math.abs(Math.PI - Task4.calculatePiMultithreaded(ITERATIONS1B, COUNT_OF_THREADS));
+
+        // Then
+        System.out.printf((ACCURACY_RESULT_TEMPLATE) + "%n", ITERATIONS10M, accuracy1);
+        System.out.printf((ACCURACY_RESULT_TEMPLATE) + "%n", ITERATIONS100M, accuracy2);
+        System.out.printf((ACCURACY_RESULT_TEMPLATE) + "%n", ITERATIONS1B, accuracy3);
+
+        assertTrue(accuracy2 < accuracy1);
+    }
 
     private @NotNull Boolean isMultiThreadedFasterThanSingleThreaded(int iterations) {
         long startTimeSingleThreaded = System.currentTimeMillis();
@@ -24,30 +43,23 @@ class Task4Test {
         long endTimeMultiThreaded = System.currentTimeMillis();
         long multiThreadedTime = endTimeMultiThreaded - startTimeMultiThreaded;
 
-        System.out.println(String.format(SINGLE_THREADED_TIME_TEMPLATE, singleThreadedTime));
-        System.out.println(String.format(MULTI_THREADED_TIME_TEMPLATE, multiThreadedTime));
+        System.out.printf((SINGLE_THREADED_TIME_TEMPLATE) + "%n", singleThreadedTime);
+        System.out.printf((MULTI_THREADED_TIME_TEMPLATE) + "%n", multiThreadedTime);
 
         double averageAcceleration = (double) singleThreadedTime / multiThreadedTime;
-        System.out.println(String.format(AVERAGE_ACCELERATION_TEMPLATE, averageAcceleration));
+        System.out.printf((AVERAGE_ACCELERATION_TEMPLATE) + "%n", averageAcceleration);
+
         return startTimeMultiThreaded > multiThreadedTime;
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {10000000, 100000000, 1000000000})
+    @ValueSource(ints = {ITERATIONS10M, ITERATIONS100M, ITERATIONS1B})
     void testThatMultiThreadedFasterThanSingleThreaded(int iterations) {
+        // Given
+        // When
         Boolean ans = isMultiThreadedFasterThanSingleThreaded(iterations);
 
+        //Then
         assertTrue(ans);
-    }
-
-    @Test
-    void testThatAccuracyIsMaintained() {
-        double accuracy1 = Math.abs(Math.PI - Task4.calculatePiMultithreaded(10000000, COUNT_OF_THREADS));
-        double accuracy2 = Math.abs(Math.PI - Task4.calculatePiMultithreaded(100000000, COUNT_OF_THREADS));
-        double accuracy3 = Math.abs(Math.PI - Task4.calculatePiMultithreaded(1000000000, COUNT_OF_THREADS));
-
-        System.out.println(String.format(ACCURACY_RESULT_TEMPLATE, 10000000, accuracy1));
-        System.out.println(String.format(ACCURACY_RESULT_TEMPLATE, 100000000, accuracy2));
-        System.out.println(String.format(ACCURACY_RESULT_TEMPLATE, 1000000000, accuracy3));
     }
 }
