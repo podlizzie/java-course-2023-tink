085a8059a6bf0e409b828993a589c6e154b25438
lzbkl <lzbkln@mail.ru>
2023-12-10 09:13:39 +0500

    correct AffineGenerator

--- src/main/java/edu/project4/transformation/AffineGenerator.java
+++ src/main/java/edu/project4/transformation/AffineGenerator.java
@@ -2,42 +2,25 @@ package edu.project4.transformation;
 
 import edu.project4.entity.Point;
 import java.awt.Color;
-import java.util.Random;
 
 public class AffineGenerator implements Transformation {
-    Random random = new Random();
-    private double a;
-    private double b;
-    private double c;
-    private double d;
-    private double e;
-    private double f;
-    private Color color;
 
-    public AffineGenerator(Color color) {
+    private final double a;
+    private final double b;
+    private final double c;
+    private final double d;
+    private final double e;
+    private final double f;
+    private final Color color;
+
+    public AffineGenerator(double a, double b, double c, double d, double e, double f, Color color) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+        this.f = f;
         this.color = color;
-        generateValidCoefficients();
-    }
-
-    private double generateValidCoefficient() {
-        return random.nextDouble() * 2 - 1;
-    }
-
-    private void generateValidCoefficients() {
-        do {
-            a = generateValidCoefficient();
-            d = generateValidCoefficient();
-        } while (a * a + d * d >= 1);
-
-        do {
-            b = generateValidCoefficient();
-            e = generateValidCoefficient();
-        } while (b * b + e * e >= 1);
-
-        do {
-            c = generateValidCoefficient();
-            f = generateValidCoefficient();
-        } while (a * a + b * b + d * d + e * e >= 1 + Math.pow(a * e - b * d, 2));
     }
 
     @Override
--- src/main/java/edu/project4/transformation/AffineUtils.java
+++ src/main/java/edu/project4/transformation/AffineUtils.java
@@ -11,20 +11,44 @@ public class AffineUtils {
     }
 
     private static final int MAX_COLOR = 255;
-    private static final int COUNT_OF_TRANSFORMATIONS = 50;
+    private static final int MAX_ATTEMPTS = 1000;
 
-    public static List<AffineGenerator> getListOfAffineTransformations() {
+    public static List<AffineGenerator> getListOfAffineTransformations(int n) {
         List<AffineGenerator> transformations = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            transformations.add(generateValidCoefficients());
+        }
+        return transformations;
+    }
+
+    private static AffineGenerator generateValidCoefficients() {
+        double a, b, c, d, e, f;
+        do {
+            a = generateValidCoefficient();
+            d = generateValidCoefficient();
+            b = generateValidCoefficient();
+            e = generateValidCoefficient();
+            c = generateValidCoefficient();
+            f = generateValidCoefficient();
+        } while ((a * a + b * b + d * d + e * e >= 1 + Math.pow(a * e - b * d, 2)) && (b * b + e * e >= 1) &&
+            (a * a + d * d >= 1));
 
         int red = ThreadLocalRandom.current().nextInt(0, MAX_COLOR + 1);
         int green = ThreadLocalRandom.current().nextInt(0, MAX_COLOR + 1);
         int blue = ThreadLocalRandom.current().nextInt(0, MAX_COLOR + 1);
 
         Color color = new Color(red, green, blue);
+        return new AffineGenerator(a, b, c, d, e, f, color);
+    }
 
-        for (int i = 0; i < COUNT_OF_TRANSFORMATIONS; i++) {
-            transformations.add(new AffineGenerator(color));
-        }
-        return transformations;
+    private static double generateValidCoefficient() {
+        int attempts = 0;
+        double coefficient;
+        do {
+            attempts++;
+            coefficient = ThreadLocalRandom.current().nextDouble(-1, 1);
+        } while (attempts < MAX_ATTEMPTS);
+        return coefficient;
     }
 }
