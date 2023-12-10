7ac20e9e073a1078b3c4593ef103c26072155914
lzbkl <lzbkln@mail.ru>
2023-12-11 02:00:05 +0500

    update StatsCollector

--- src/main/java/edu/hw9/task1/StatsCollector.java
+++ src/main/java/edu/hw9/task1/StatsCollector.java
@@ -1,21 +1,51 @@
 package edu.hw9.task1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class StatsCollector {
-    private final Map<String, MetricAggregator> metrics = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, List<Double>> metricsMap = new ConcurrentHashMap<>();
 
     public void push(String metricName, double[] values) {
-        MetricAggregator aggregator = metrics.computeIfAbsent(metricName, k -> new MetricAggregator());
-        aggregator.push(values);
+        metricsMap.compute(metricName, (key, list) -> {
+            if (list == null) {
+                list = new ArrayList<>();
+            }
+            Arrays.stream(values).forEach(list::add);
+            return list;
+        });
     }
 
     public Map<String, Map<MetricType, Double>> stats() {
-        Map<String, Map<MetricType, Double>> statsMap = new ConcurrentHashMap<>();
-        for (Map.Entry<String, MetricAggregator> entry : metrics.entrySet()) {
-            statsMap.put(entry.getKey(), entry.getValue().getStatistics());
-        }
-        return statsMap;
+        Map<String, Map<MetricType, Double>> stats = new HashMap<>();
+        metricsMap.forEach((metricName, values) -> {
+            double sum = 0;
+            double min = Double.MAX_VALUE;
+            double max = -Double.MAX_VALUE;
+
+            for (double value : values) {
+                sum += value;
+                if (value < min) {
+                    min = value;
+                }
+                if (value > max) {
+                    max = value;
+                }
+            }
+
+            double average = sum / values.size();
+            Map<MetricType, Double> metricStats = new HashMap<>();
+            metricStats.put(MetricType.SUM, sum);
+            metricStats.put(MetricType.AVERAGE, average);
+            metricStats.put(MetricType.MIN, min);
+            metricStats.put(MetricType.MAX, max);
+
+            stats.put(metricName, metricStats);
+        });
+        return stats;
     }
 }
