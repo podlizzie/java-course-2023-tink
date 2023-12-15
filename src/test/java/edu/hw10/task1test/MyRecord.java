a470aa9de71c153ab29034f3230c151ef55c986f
lzbkl <lzbkln@mail.ru>
2023-12-16 02:51:02 +0500

    add new annotations to POJO && MyRecord

--- src/test/java/edu/hw10/task1test/MyRecord.java
+++ src/test/java/edu/hw10/task1test/MyRecord.java
@@ -2,8 +2,7 @@ package edu.hw10.task1test;
 
 import edu.hw10.task1.annotations.Max;
 import edu.hw10.task1.annotations.Min;
-import edu.hw10.task1.annotations.NotNull;
 
-public record MyRecord(@NotNull @Min(minVal = 45) int weight,
+public record MyRecord(@Max(maxVal = 55) @Min(minVal = 45) int weight,
                        @Max(maxVal = 160) int height) {
 }
--- src/test/java/edu/hw10/task1test/POJO.java
+++ src/test/java/edu/hw10/task1test/POJO.java
@@ -22,23 +22,11 @@ public class POJO {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public int getAge() {
         return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 }
